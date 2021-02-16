337f5d716640e416d5ed2b47e352c707a4f70bfa
Jervis Thompson <jervo@mac.com>
2021-02-16 16:47:50 -0500

    Update
    Week 5 and 5 class lecture samples

--- idm231-wk05-dom/js/main.js
+++ idm231-wk05-dom/js/main.js
@@ -5,3 +5,44 @@ function fade2Content() {
   console.log('fade2Content called');
   document.body.classList.remove("blackScreen");
 }
+
+const pisObj = document.getElementById('pis');
+const scoObj = document.getElementById('sco');
+const aquObj = document.getElementById('aqu');
+
+const displayObj = document.getElementById('display');
+
+pisObj.addEventListener('click', function (evt) {
+  console.log('pis button clicked: ' + evt.target.id);
+  const idName = evt.target.id;
+  userPicked(idName);
+});
+
+scoObj.addEventListener('click', function (evt) {
+  console.log('sco button clicked: ' + evt.target.id); 
+  const idName = evt.target.id;
+  userPicked(idName);
+});
+
+aquObj.addEventListener('click', function (evt) {
+  console.log('aqu button clicked: ' + evt.target.id);
+  const idName = evt.target.id;
+  userPicked(idName);
+});
+
+function userPicked(whichOne) {
+  console.log('userPicked called: ' + whichOne);
+  switch (whichOne) {
+    case 'pis':
+      displayObj.src = 'img/ful_pisces.png';
+      break;
+    case 'sco':
+      displayObj.src = 'img/ful_scorpio.png';
+      break;  
+    case 'aqu':
+      displayObj.src = 'img/ful_aquarius.png';
+      break;
+    default:
+      displayObj.src = 'img/ful_blank.png';
+  }
+}
